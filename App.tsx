b004fd7fa3fb96c49504fa9e8b5c60bb6b78e01f
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-07-18 02:34:00 -0300

    feat: trasnfor project in typescript files

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { THEME } from "./src/theme";
+
+import { SignIn } from "./src/screens";
+import { Loading } from "./src/components";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
