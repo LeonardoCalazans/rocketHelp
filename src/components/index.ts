7fd9372e58953ca57773c121840e528cc2a36f21
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-07-20 19:14:16 -0300

    feat: created component CardDetails

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Loading } from './Loading';
 export { default as Header } from './Header';
 export { default as Order } from './Order';
 export { default as Filter } from './Filter';
+export { default as CardDetails } from './CardDetails';
